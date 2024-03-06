83b7d24ee90f38f75839106b1d59c9e5b89ecc16
DOggg <alanchacko42@gmail.com>
2024-03-06 10:04:16 +0800

    first attemtent

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+include("header.php");
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+  <!-- Hero Section Begin -->
+    <section class="hero-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="hero-text">
+                        <h1>St.Mary's forane church kanjoor</h1>
+                        <p>St. Sebastian is known for his quick reply to those who 
+                           call on him in distress especially those who seek the assistance
+                           of the saint for deliverance from incurable diseases, epidemics, and shattering problems of life</p>
+                        <a href="#" class="primary-btn">Discover Now</a>
+                    </div>
+                </div>
+                <!--<div class="col-xl-4 col-lg-5 offset-xl-2 offset-lg-1">
+                    <div class="booking-form">
+                        <h3>Booking  Request</h3>
+                        <form action="#">
+                            
+
+                            <div class="check-date">
+                                <label for="date-in">Check In:</label>
+                                <input type="text" class="date-input" id="date-in">
+                                <i class="icon_calendar"></i>
+                            </div>
+                            <div class="check-date">
+                                <label for="date-out">Check Out:</label>
+                                <input type="text" class="date-input" id="date-out">
+                                <i class="icon_calendar"></i>
+                            </div>
+                            <div class="select-option">
+                                <label for="guest">Guests:</label>
+                                <select id="guest">
+                                    <option value="">2 Adults</option>
+                                    <option value="">3 Adults</option>
+                                </select>
+                            </div>
+                            <div class="select-option">
+                                <label for="room">Room:</label>
+                                <select id="room">
+                                    <option value="">1 Room</option>
+                                    <option value="">2 Room</option>
+                                </select>
+                            </div>
+                            <button type="submit">Check Availability</button>
+                        </form>
+                    </div>
+                </div>-->
+            </div>
+        </div>
+        <div class="hero-slider owl-carousel">
+             <div class="hs-item set-bg" data-setbg="img/hero/DJI01462.jpg"></div>
+            <div class="hs-item set-bg" data-setbg="img/hero/pallimeda.jpg"></div>
+            <div class="hs-item set-bg" data-setbg="img/hero/DJI01520.jpg"></div>
+        </div>
+    </section>
+    <!-- Hero Section End -->
+
+    
+   
+</body>
+</html>
+<?php
+include("footer.php");
+?>
